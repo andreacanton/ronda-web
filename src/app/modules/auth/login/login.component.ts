be86aa1cad3e8ea65e0d9ee12b1ae9a9314eb120
Andrea Canton <andrea.canton@gmail.com>
2021-01-04 10:52:53 +0100

    fix login styles

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -4,6 +4,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
   public loginForm: FormGroup;

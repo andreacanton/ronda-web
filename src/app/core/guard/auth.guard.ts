95d771a837fd7e41216cd82c1053a067820237fc
Andrea Canton <andrea.canton@gmail.com>
2021-04-17 13:56:58 +0200

    add refresh token process

--- src/app/core/guard/auth.guard.ts
+++ src/app/core/guard/auth.guard.ts
@@ -9,6 +9,7 @@ import {
 import { AuthService } from '@core/services/auth.service';
 import { TokenService } from '@core/services/token.service';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -23,11 +24,13 @@ export class AuthGuard implements CanActivate {
     | Promise<boolean | UrlTree>
     | boolean
     | UrlTree {
-    if (this.authService.isLoggedIn()) {
-      return true;
-    } else {
-      this.router.navigate(['/auth/login']);
-      return false;
-    }
+    return this.authService.isLoggedIn().pipe(
+      map((isLoggedIn) => {
+        if (!isLoggedIn) {
+          this.router.navigate(['/auth/login']);
+        }
+        return isLoggedIn;
+      })
+    );
   }
 }
--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -27,13 +27,7 @@ export class AuthService {
       })
       .pipe(
         map((response) => {
-          const { access_token, refresh_token } = response;
-          const payload = this.jwtHelper.decodeToken<JwtPayload>(access_token);
-          this.tokenService.setExpiration(
-            parse(payload.exp.toString(), 't', new Date())
-          );
-          this.tokenService.setAccessToken(access_token);
-          this.tokenService.setRefreshToken(refresh_token);
+          this.setTokens(response);
           return {
             success: true,
             errors: null,
@@ -70,10 +64,41 @@ export class AuthService {
     }
   }
 
-  public isLoggedIn(): boolean {
-    return (
-      this.getUserInfo() !== null &&
-      this.tokenService.getExpiration() > new Date()
+  public refreshToken(): Observable<boolean> {
+    return this.apiClient
+      .post<{ access_token: string; refresh_token: string }>('auth/refresh', {
+        refreshToken: this.tokenService.getRefreshToken(),
+      })
+      .pipe(
+        map((response) => {
+          this.setTokens(response);
+          return true;
+        }),
+        catchError((errors) => {
+          return of(false);
+        })
+      );
+  }
+
+  private setTokens(response: { access_token: string; refresh_token: string }) {
+    const { access_token, refresh_token } = response;
+    const payload = this.jwtHelper.decodeToken<JwtPayload>(access_token);
+    this.tokenService.setExpiration(
+      parse(payload.exp.toString(), 't', new Date())
     );
+    this.tokenService.setAccessToken(access_token);
+    this.tokenService.setRefreshToken(refresh_token);
+  }
+
+  public isLoggedIn(): Observable<boolean> {
+    if (this.tokenService.getExpiration() < new Date()) {
+      return this.refreshToken().pipe(
+        map(() => {
+          return this.getUserInfo() !== null;
+        })
+      );
+    } else {
+      return of(this.getUserInfo() !== null);
+    }
   }
 }
